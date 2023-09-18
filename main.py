9ed44a918cadb5026c79fb32c7cdabd9507bae08
alschultz2 <alschultz2@mail.fhsu.edu>
2023-09-17 19:32:16 -0500

    Setup charts folder to build if missing and added to gitignore

--- main.py
+++ main.py
@@ -5,14 +5,15 @@
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 #(5/5 points) Initial comments with your name, class and project at the top of your .py file. #Done
 #(5/5 points) Proper import of packages used.   #Done
-#(10/10 points) Store this information in a list that you will convert to a array in NumPy.
+#(10/10 points) Store this information in a list that you will convert to a array in NumPy. #Done
 #(10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points. #Done
-#(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-#(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+#(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file. #Done
+#(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often! #Done
 #(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 #(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown
 
@@ -37,6 +38,11 @@ def getClosing(ticker):
 
     return closingList
 
+try:
+    # Create our charts folder
+    Path("charts").mkdir()
+except FileExistsError:
+    pass
 
 stocks = ["MSFT", "AAPL", "GME", "SONY", "META"]
 
